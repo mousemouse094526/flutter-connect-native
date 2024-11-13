fd6d34cdada87f7426ab833c2274ebb1a24c4f60
jarukit bunchan <jarukit.bunchan@gmail.com>
2024-11-13 21:10:27 +0700

    add function on kotlin and call in flutter

--- android/app/src/main/kotlin/com/example/flutter_connect_native/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_connect_native/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.example.flutter_connect_native
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+
+    private val CHANNEL = "com.example/native_bridge"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getNativeData") {
+                val nativeData = getNativeData()
+                result.success(nativeData)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getNativeData(): String {
+        return "BeLnwZa007"
+    }
+}
+ 
